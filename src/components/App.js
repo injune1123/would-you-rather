220fefb98a7de56a494410580600a4413bf0be29
injune1123 <injune1123@gmail.com>
2020-09-17 21:26:50 -0400

    Fix dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 class App extends Component {
   componentDidMount(){
-    this.props.dispatch(handleInitialData)
+    this.props.dispatch(handleInitialData())
   }
 
   render (){
